9cc1368ac67176245e10536d395ad613c23af4d5
sai-yaswanth <yaswanthnaidu679@gmail.com>
2025-03-08 10:55:23 +0530

    hi

--- app.py
+++ app.py
@@ -5,16 +5,11 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-SAI-YASHWANTH
-@app.route("/predict",methods=["GET","POST"])
-def predict():
-    if request.method=="POST": 
 
 
 @app.route("/predict",methods=["GET","POST"])
 def predict():
     if request.method=="POST":
-main
         msg=request.form.get("message")
         print(msg)
         ob=TextToNum(msg)
@@ -35,10 +30,6 @@ main
     else:
         return render_template("predict.html")
 
- SAI-YASHWANTH
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5151)
-=======
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5050)
-    main
+    app.run(host="0.0.0.0",port=5151)
+
